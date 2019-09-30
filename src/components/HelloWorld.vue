db7ed0c538c5faa71ab5a6d9a5fe540e4783b7b5
PangHu200Catties <1007290345@qq.com>
2019-09-30 13:40:38 +0800

    前端

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>记得先去安装下Elementui  npm i element-ui -S 还有 npm install axios,记得导包 import axios from 'axios'</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
