6410919ec6f754a5f349b7adde984f249a2c29dd
PangHu200Catties <1007290345@qq.com>
2019-10-08 11:39:33 +0800

    吴佐彬 前端首页

---
+++ src/components/homepage.vue
@@ -0,0 +1,284 @@
+<template>
+  <div class="body">
+    <el-carousel indicator-position="outside">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  <div class="showbox">
+    <ul class="showboxul">
+      <li class="boxli">
+        <img src="../assets/icon1.png">
+        <p>安全SECURE</p>
+      </li>
+      <li class="boxli">
+        <img src="../assets/icon2.png">
+        <p>舒适COMFORT</p>
+      </li>
+      <li class="boxli">
+        <img src="../assets/icon3.png">
+        <p>贴心WARM</p>
+      </li>
+      <li class="boxli">
+        <img src="../assets/icon4.png">
+        <p>社交SOCIAL</p>
+      </li>
+    </ul>
+  </div>
+  <div class="hotrent">
+    <div class="hotrenttitle">
+      <p>热门租房</p>
+      <span>POPULAR APARTMENTS</span>
+    </div>
+    <ul>
+      <li>
+        <img src="../assets/pic1.jpg" style="width: 380px;height: 248px;border-radius: 5px;">
+        <div class="hotmsg">
+          <span class="hotname">驻马店</span>
+          <span class="hotmoney">
+            <font>￥</font>
+            <em>2620</em>/月起
+          </span>
+          <p>
+            南京市股骨干心机女
+          </p>
+        </div>
+      </li>
+      <li>
+        <img src="../assets/pic1.jpg" style="width: 380px;height: 248px;border-radius: 5px;">
+        <div class="hotmsg">
+          <span class="hotname">驻马店</span>
+          <span class="hotmoney">
+            <font>￥</font>
+            <em>2620</em>/月起
+          </span>
+          <p>
+            南京市股骨干心机女
+          </p>
+        </div>
+      </li>
+      <li>
+        <img src="../assets/pic1.jpg" style="width: 380px;height: 248px;border-radius: 5px;">
+        <div class="hotmsg">
+          <span class="hotname">驻马店</span>
+          <span class="hotmoney">
+            <font>￥</font>
+            <em>2620</em>/月起
+          </span>
+          <p>
+            南京市股骨干心机女
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+    <div class="boutique">
+      <div class="hotrenttitle">
+        <p>精品系列</p>
+        <span>BOUTIQUE SERIES</span>
+      </div>
+      <ul>
+        <li>
+          <a href=""><img src="../assets/pic2.jpg" style="width: 280px;height: 380px;border-radius: 5px;"></a>
+        </li>
+        <li>
+          <a href=""><img src="../assets/pic2.jpg" style="width: 280px;height: 380px;border-radius: 5px;"></a>
+        </li>
+        <li>
+          <a href=""><img src="../assets/pic2.jpg" style="width: 280px;height: 380px;border-radius: 5px;"></a>
+        </li>
+        <li>
+          <a href=""><img src="../assets/pic2.jpg" style="width: 280px;height: 380px;border-radius: 5px;"></a>
+        </li>
+      </ul>
+    </div>
+    <div class="rent">
+      <div class="hotrenttitle">
+        <p>区域找房</p>
+      </div>
+      <ul>
+        <li>
+          <a href=""><img src="../assets/pic2.jpg" style="float: left;width: 210px;height: 150px;position: relative;"> </a>
+          <div class="rent_info">
+            <a href="" target="_blank">
+               <b class="rent_title">雁塔区 南窑头 89排</b>
+            </a>
+            <p class="rent_details"  >
+              <b class="rent_title" style="font-weight: normal">3</b>室<b class="rent_title" style="font-weight: normal">2</b>厅&nbsp
+              <span>|</span>
+              &nbsp<b class="rent_title" style="font-weight: normal">25</b>平米&nbsp
+              <span>|</span>
+              &nbsp<b class="rent_title" style="font-weight: normal">2</b>楼
+            </p>
+            <address class="rent_details" >
+              雁塔区 南窑头 89排9号
+            </address>
+          </div>
+          <div class="rent_price">
+            <strong>
+              <b class="rent_title" style="color: #eb5f00;text-align: right;">700</b><span style="color: #eb5f00;text-align: right">/月</span>
+            </strong>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'homepage',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+<style>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+  .body{
+    margin:0 auto;
+    width:1180px;
+  }
+  .showbox{
+
+    margin:0 auto;
+    width:1180px;
+    height: 278px;
+  }
+  .showboxul{
+    list-style: none;
+    margin:0;padding:0;
+  }
+  .boxli{
+    float: left;
+    text-align: center;
+    width: 295px;
+    height: 156px;
+    padding-top: 60px;
+  }
+  .boxli p{
+    font-size: 24px;
+    color: #828282;
+    margin-top: 40px;
+    font-weight: 300;
+  }
+
+  .hotrent{
+    height: 486px;
+  }
+  .hotrenttitle p{
+    font-size: 40px;
+    color: #222222;
+    display: inline-block;
+    font-weight: normal;
+  }
+  .hotrenttitle span{
+    font-size: 30px;
+    color: #dcdcdc;
+    display: inline-block;
+    margin-left: 50px;
+  }
+  .hotrent  ul li{
+    width: 380px;
+    height: 338px;
+    float: left;
+    margin: 0 4px;
+    border:solid 1px #ddd;
+  }
+  .hotmsg{
+    overflow: hidden;
+    margin-top: 15px;
+    width:380px;
+    height:56px;
+
+  }
+  .hotname{
+    font-size: 22px;
+    color: #222222;
+    float: left;
+    margin-top: 5px;
+  }
+  .hotmoney{
+    font-size: 16px;
+    color: #c58d5d;
+    display: inline-block;
+    float: right;
+    margin-top: 2px;
+  }
+  font{
+     font-size:20px;
+  }
+  em{
+    font-size: 30px;
+    font-style: normal;
+  }
+  .hotmsg p{
+    font-size: 16px;
+    color: #828282;
+    display: block;
+    width: 100%;
+    margin-top:33px;
+    white-space: nowrap;
+
+  }
+  .boutique{
+    height:520px;
+
+  }
+  .boutique li{
+    float: left;
+    margin:0 7px;
+  }
+  .rent{
+
+  }
+  .rent ul li{
+
+    height: 176px;
+
+    border-top: 1px dashed #e6e6e6;
+    cursor: pointer;
+  }
+  .rent_info{
+    float: left;
+    padding-left: 20px;
+    width: 865px;
+    overflow: hidden;
+    font-size: 14px;
+    color: #797979;
+
+  }
+  .rent_title{
+    height: 34px;
+    line-height: 40px;
+    margin-bottom: 6px;
+    display: inline;
+    font-size: 25px;
+  }
+  .rent_details{
+    width: 440px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    height: 30px;
+    line-height: 30px;
+
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import homepage from '@/components/homepage'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'homepage',
+      component: homepage
     }
   ]
 })
